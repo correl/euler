82c0ac06721de37f780a5edb12c36d8f408569eb
Correl Roush <correl@gmail.com>
2010-04-12 15:53:03 +0000

    Problem 005
    git-svn-id: file:///srv/svn/euler@23 e5f4c3ec-3c0c-11df-b522-21efaa4426b5

---
+++ 005.py
@@ -0,0 +1,12 @@
+def divisible(n):
+    i = 0
+    while True:
+        i = i + 1
+        x = n * i
+        for ii in range(n, 0, -1):
+            if x % ii != 0:
+                break
+        if ii == 1:
+            return x
+
+print 'Smallest number: ', divisible(20)
