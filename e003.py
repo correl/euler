9d98a914f506b116b7f138af6be4ef1d3eed293a
Correl Roush <correl@gmail.com>
2010-04-12 15:53:13 +0000

    Renamed prime factor function
    git-svn-id: file:///srv/svn/euler@34 e5f4c3ec-3c0c-11df-b522-21efaa4426b5

--- e003.py
+++ e003.py
@@ -1,12 +1,12 @@
-def factor(n):
+def pfactor(n):
     i = 2
     while (n % i != 0 and i < n):
         i = i + 1
     if i == n:
         return [n]
-    p = factor(n // i)
+    p = pfactor(n // i)
     p.append(i)
     return sorted(p, reverse=True)
 
 print 'Prime factors of 600851475143:'
-print factor(600851475143)
+print pfactor(600851475143)
